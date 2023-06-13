1bf9c886c5b9b806577c2c04e9550d37103f9682
Hari <sandhari@deloitte.com>
2023-06-13 15:20:14 +0530

    Bug fix regarding data fetching in homepage

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -4,8 +4,26 @@ import { connectToDB } from "@utils/database";
 export const GET = async (req, res) => {
   try {
     await connectToDB();
-    const getPrompts = await Prompt.find({}).populate("creator");
-    return new Response(JSON.stringify(getPrompts), { status: 200 });
+    const prompts = await Prompt.find().populate({
+      path: "creator",
+    });
+
+    const response = new Response(JSON.stringify(prompts), {
+      status: 200,
+    });
+
+    // Add a unique identifier to the URL to force a cache-busting reload
+    const url = new URL(req.url);
+    url.searchParams.set("t", Date.now());
+    response.headers.set(
+      "Cache-Control",
+      "no-cache, no-store, must-revalidate"
+    );
+    response.headers.set("Pragma", "no-cache");
+    response.headers.set("Expires", "0");
+    response.headers.set("Location", url.toString());
+
+    return response;
   } catch (error) {
     return new Response("Failed to fetch all prompts", { status: 500 });
   }
